9249c7caff5ac3cb606e30e10e7cc00c5018c239
Hector Vargas <vargash1@wit.edu>
2014-12-09 18:38:01 -0500

    first commit to get things going!

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the board game battleship in x86_64 assembly.
+You will need the Irvrin32 Libary to run this 
---
+++ bShip.asm
@@ -0,0 +1,133 @@
+; @Author: vargash1
+; @Name:   Vargas, Hector
+; @Email:  vargash1@wit.edu
+; @Date:   2014-12-07 13:04:53
+; @Last Modified by:   vargash1
+; @Last Modified time: 2014-12-09 10:13:30
+INCLUDE Irvine32.inc
+;-------------------------------------------
+;ships
+;Carrier        5
+    Carrier     STRUCT
+        x0      BYTE    0
+        x1      
+    Carrier     ENDS
+;BattleShip     4
+    BattleShip  STRUCT
+
+    BattleShip  ENDS
+;Destroyer      3
+    Destroyer   STRUCT
+    Destroyer   ENDS
+;Submarine      3
+    Submarine   STRUCT
+    Submarine   ENDS
+;Patrol         2
+    Patrol      STRUCT
+    Patrol      ENDS
+;-------------------------------------------
+;board 
+    Gameboard  STRUCT
+        board_x     BYTE    1,2,3,4,5,6,7,8,9,10
+        board_y     BYTE    "ABCDEFGHI"0
+        vtab        BYTE    '|'
+        
+
+    Gameboard  ENDS
+;-------------------------------------------
+;player move 
+    Event STRUCT
+        miss        BYTE    'O'
+        hit         BYTE    'X'
+    Event ENDS      
+;-------------------------------------------
+;user instructions
+    Instructions STRUCT
+        move_a      BYTE    '0'
+        move_b      BYTE    'Z'
+        begin       BYTE    "Welome to Battleship!",0
+        ships       BYTE    "Ships have been randomized on the field! Good Luck!",0
+        move_x      BYTE    "You will enter a number on the X axis",0
+        move_y      BYTE    "Followed by a letter on the Y axis!",0    
+        prompt      BYTE    "> ",0
+    Instructions ENDS 
+;-------------------------------------------
+;fun banner
+    Banner       STRUCT
+        row1        BYTE    "     ____        __  __  __    _____ __    _          _          __  ______   _____ __  ___",0
+        row2        BYTE    "    / __ )____ _/ /_/ /_/ /__ / ___// /_  (_)___     (_)___     /  |/  /   | / ___//  |/  /",0
+        row3        BYTE    "   / __  / __ `/ __/ __/ / _ \\__ \/ __ \/ / __ \   / / __ \   / /|_/ / /| | \__ \/ /|_/ / ",0
+        row4        BYTE    "  / /_/ / /_/ / /_/ /_/ /  __/__/ / / / / / /_/ /  / / / / /  / /  / / ___ |___/ / /  / /  ",0
+        row5        BYTE    " /_____/\__,_/\__/\__/_/\___/____/_/ /_/_/ .___/  /_/_/ /_/  /_/  /_/_/  |_/____/_/  /_/   ",0
+        row6        BYTE    "                                        /_/                                                ",0
+    Banner       ENDS
+;-------------------------------------------
+.data
+    ship_carrier    Carrier <>
+    user_intruc     Instructions<>
+    ban             Banner<>
+.code
+main             PROC
+    call    print_banner
+    call    user_intructions
+    INVOKE  ExitProcess,0
+main             ENDP
+;-------------------------------------------
+; greets and prints instructions to the user
+user_intructions PROC
+    mov     eax, green + (black * 16)
+    call    SetTextColor
+    mov     edx,OFFSET user_intructions.begin
+    call    WriteString
+    call    crlf
+    mov     edx,OFFSETuser_intructions.ships   
+    call    WriteString
+    call    crlf
+    mov     edx,OFFSET user_intructions.move_x
+    call    WriteString
+    call    crlf
+    mov     edx,OFFSET user_intructions.move_y
+    call    WriteString
+    call    crlf
+    ret
+user_intructions ENDP
+;-------------------------------------------
+; draw the board out
+draw_board       PROC
+    call    crlf
+    ret 
+draw_board       ENDP
+;-------------------------------------------
+; TODO randomize ships here
+randomize_s     PROC
+    ret
+randomize_s     ENDP
+;-------------------------------------------
+; TODO get user turn
+get_turn        PROC
+    call    GetXY
+    ret
+get_turn        ENDP
+;-------------------------------------------
+; TODO update board
+update_board    PROC
+    ret
+update_board    ENDP
+;-------------------------------------------
+print_banner    PROC
+    mov    edx,OFFSET ban.row1
+    call   WriteString
+    mov    edx,OFFSET ban.row2
+    call   WriteString
+    mov    edx,OFFSET ban.row3
+    call   WriteString
+    mov    edx,OFFSET ban.row4
+    call   WriteString
+    mov    edx,OFFSET ban.row5
+    call   WriteString
+    mov    edx,OFFSET ban.row6
+    call   WriteString
+    call   crlf
+print_banner    ENDP
+end     main
+
